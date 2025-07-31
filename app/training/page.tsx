bbc3d4d15b19dbe34915ca881c2c0cb85639bbc3
aghilliotti <aghilliotti@gmail.com>
2025-07-30 22:42:31 -0400

    Update page.tsx

--- app/training/page.tsx
+++ app/training/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const objections = {
   price: "The building across the street is offering two months free and is cheaper overall.",
